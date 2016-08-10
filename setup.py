f44250cd2cd9257dbf525dfd41309dca3b2b489b
Tom Vogels <tom@harrys.com>
2016-08-10 16:29:01 -0400

    Pull requirements and setup into toplevel directory
    Not building inside the python source code simplifies managing bulid directories
    and eventually publication of the python code into S3

--- setup.py
+++ setup.py
@@ -9,13 +9,12 @@ setup(
     license="MIT",
     keywords="redshift etl",
     url="https://github.com/harrystech/harrys-redshift-etl",
-    packages=find_packages(),
-    package_data={'etl': ["config/*"]},
-    scripts=[
-        "scripts/submit_arthur.sh"
-    ],
+    packages=["etl"],
+    package_dir={"": "python"},
+    package_data={"python": ["etl/config/*"]},
+    scripts=["python/scripts/submit_arthur.sh"],
     entry_points={
         # NB The script must end in ".py" so that spark submit accepts it as a Python script.
-        'console_scripts': ['arthur.py=etl.commands:run_arg_as_command']
+        "console_scripts": ["arthur.py=etl.commands:run_arg_as_command"]
     }
 )
