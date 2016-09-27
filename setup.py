240a249d2ecaf41f24b395b7ec3077c03ad9e513
Benson Tucker <bhtucker@users.noreply.github.com>
2016-09-27 13:51:35 -0400

    Update setup.py
    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="redshift-etl",
-    version="0.10.3",
+    version="0.10.4",
     author="Harry's Data Engineering and Contributors",
     description="ETL code to ferry data from PostgreSQL databases to Redshift cluster",
     license="MIT",
