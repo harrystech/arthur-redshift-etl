fd1af3e59ccded5358ba2960735a2df0de84012e
Janahan Sivaraman <sjanahan@github.com>
2016-11-01 08:30:48 -0400

    BIDW-281 Add cronut ping after bootstrap
    In an effort to hit the 8am data-is-fresh requirement, we'd
    like to be notified by cronut if the bootstrap of the EMR
    cluster doesn't happen for any reason.

--- bin/ping_cronut.sh
+++ bin/ping_cronut.sh
@@ -2,9 +2,11 @@
 
 # This script expects these environment variables:
 #   CRONUT_API_TOKEN - identify this project
-#   CRONUT_PUBLIC_ID - identify this particular schedule
 #   CRONUT_PUBLIC_KEY - encrypt the public id in transit
 #   CRONUT_BASE_URL - location of the CRONUT service
+#
+# This script expects one command line argument which is the
+# Cronut public_id to identify the desired schedule
 # See also https://github.com/harrystech/cronut
 
 source /tmp/redshift_etl/config/cronut_env.sh
@@ -12,6 +14,8 @@ source /tmp/redshift_etl/config/cronut_env.sh
 CRONUT_PUBLIC_KEY_FILE="/tmp/cronut_pub_$$"
 trap "rm \"$CRONUT_PUBLIC_KEY_FILE\"" EXIT
 echo -n "$CRONUT_PUBLIC_KEY" > "$CRONUT_PUBLIC_KEY_FILE"
+
+CRONUT_PUBLIC_ID=${1?'Missing Cronut public_id'}
 
 CURRENT_TIME=`date '+%s'`
 echo -n "$CURRENT_TIME-$CRONUT_PUBLIC_ID" |
