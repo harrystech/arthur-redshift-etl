4e9646fbcdd7cc67897cb65f5e2d309b85b0cead
Chris Clouten <chris@harrys.com>
2017-01-11 16:15:52 -0500

    Bumping version to 0.15.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="redshift-etl",
-    version="0.14.4",
+    version="0.15.0",
     author="Harry's Data Engineering and Contributors",
     description="ETL code to ferry data from PostgreSQL databases to Redshift cluster",
     license="MIT",
