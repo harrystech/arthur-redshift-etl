78305255ddf0b5fb38dd77d7dbe23119ffd0bf29
Tom Vogels <tom@harrys.com>
2017-04-02 11:49:26 -0400

    Stop checking for unbound variables
    Bootstrap fails with "venv/bin/activate: line 8: _OLD_VIRTUAL_PATH: unbound variable" when using "set -u".

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="redshift-etl",
-    version="0.21.2",
+    version="0.21.4",
     author="Harry's Data Engineering and Contributors",
     description="ETL code to ferry data from PostgreSQL databases to Redshift cluster",
     license="MIT",
