5b46cd3fb2e9d70b584477c3854fcc1758ac3efe
Tom Vogels <tom@harrys.com>
2017-04-04 21:03:13 -0400

    Allow cronut_public_ids.sh instead of cronut_env.sh

--- bin/ping_cronut.sh
+++ bin/ping_cronut.sh
@@ -1,19 +1,19 @@
 #! /bin/bash
 
 # This script sends a "ping" by hitting the Cronut app with a POST request.
-# See also https://github.com/harrystech/cronut
+# See also https://github.com/harrystech/cronut for our dead-man switch service.
 
 # This script expects these environment variables to be set to connect to Cronut:
 #   CRONUT_BASE_URL - location of the CRONUT service
 #   CRONUT_API_TOKEN - identify this project
 #   CRONUT_PUBLIC_KEY - encrypt the public id in transit
 #
-# To identify the desired schedule, the environment variables must contain
-# one that looks like "CRONUT_<command line arg>".
-# For example "ping_cronut.sh WakeUp" will look for the public ID in CRONUT_WakeUp
+# To identify the desired schedule (using its public ID), the environment variables
+# must contain one that looks like "CRONUT_<command line arg>".
+# For example,  "ping_cronut.sh WAKEUP" will look for the public ID in CRONUT_WAKEUP.
 #
 # Note that the "name" here simply refers to the command line argument and
-# need not be related to the name chosen in the Cronut app.
+# need not be related to the name chosen in the Cronut service.
 
 if [[ $# -lt 1 || "$1" = "-h" ]]; then
     echo "Usage: $0 <Name of Cronut Public ID in env file>"
@@ -22,8 +22,11 @@ fi
 
 set -e
 
-source /tmp/redshift_etl/config/credentials.sh
-source /tmp/redshift_etl/config/cronut_env.sh
+for CREDENTIALS in credentials.sh cronut_env.sh cronut_public_ids.sh; do
+    if [ -r "/tmp/redshift_etl/config/$CREDENTIALS" ]; then
+        source "/tmp/redshift_etl/config/$CREDENTIALS"
+    fi
+done
 
 NAME="CRONUT_$1"
 CRONUT_PUBLIC_ID=${!NAME}
