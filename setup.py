2614fb75be92609e04562ef9bcd0fe1f1d7beb5c
Tom Vogels <tom@harrys.com>
2017-05-25 09:19:38 -0400

    Resolve merge conflicts

--- setup.py
+++ setup.py
@@ -10,14 +10,17 @@ setup(
     keywords="redshift postgresql etl extract transform load",
     url="https://github.com/harrystech/harrys-redshift-etl",
     package_dir={"": "python"},
-    packages=find_packages('python'),
+    packages=find_packages("python"),
     package_data={
         "etl": [
             "assets/*",
             "config/*"
         ]
     },
-    scripts=["python/scripts/submit_arthur.sh"],
+    scripts=[
+        "python/scripts/submit_arthur.sh",
+        "python/scripts/re_run_partial_pipeline.py"
+    ],
     entry_points={
         # NB The script must end in ".py" so that spark submit accepts it as a Python script.
         "console_scripts": ["arthur.py=etl.commands:run_arg_as_command"]
