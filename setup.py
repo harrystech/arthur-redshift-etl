1e4ef8bb6559a42b28de80ff5a97af501cc522de
Benson Tucker <benson@chatid.com>
2017-09-17 18:01:48 -0400

    update setup.py for version 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="redshift_etl",
-    version="1.2.1",
+    version="1.2.2",
     author="Harry's Data Engineering and Contributors",
     description="ETL code to ferry data from PostgreSQL databases or S3 files to Redshift clusters",
     license="MIT",
