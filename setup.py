91df19f714aa6ff8f4e53f6330f32adfea1551bc
Tom Vogels <thomas.vogels@harrys.com>
2019-03-27 06:33:20 -0700

    Bump version to v1.8.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="redshift_etl",
-    version="1.8.4",
+    version="1.8.5",
     author="Harry's Data Engineering and Analytics Engineering",
     description="ETL code to ferry data from PostgreSQL databases or S3 files to Redshift clusters",
     license="MIT",
