5e8af6468e05deb7b392100f78dfabcbb489c90c
Tom Vogels <thomas.vogels@harrys.com>
2020-02-08 05:13:18 -0500

    Add warning about locally modified files

--- bin/release_version.sh
+++ bin/release_version.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-RELEASE_PATH="python/etl/config/release.txt"
+RELEASE_FILE="python/etl/config/release.txt"
+TEMP_RELEASE_FILE="/tmp/release_version_${USER-nobody}$$.txt"
 
-set -eu
+set -o errexit -o nounset
 
 if [[ $# -gt 0 ]]; then
     cat <<EOF
 
 Usage: `basename $0`
 
-This will update current version information in '$RELEASE_PATH'.
+This will update current version information in '$RELEASE_FILE'.
 
 EOF
     exit 0
@@ -20,33 +21,33 @@ if ! type -a git >/dev/null 2>&1 ; then
     exit 1
 fi
 
-# We add the latest commit hash to the release file which is misleading if we're pulling in modified files.
-if git status --porcelain 2>/dev/null | egrep '^ M|^M' >/dev/null; then
-    echo "ERROR Not all of your changes have been committed!" >&2
-    exit 1
-fi
+> "$TEMP_RELEASE_FILE"
+trap "rm \"$TEMP_RELEASE_FILE\"" EXIT
 
-RELEASE_FILE="/tmp/upload_env_release_${USER-nobody}$$.txt"
-> "$RELEASE_FILE"
-trap "rm \"$RELEASE_FILE\"" EXIT
-
-echo "toplevel=`git rev-parse --show-toplevel`" >> "$RELEASE_FILE"
+echo "toplevel=`git rev-parse --show-toplevel`" >> "$TEMP_RELEASE_FILE"
 
 GIT_COMMIT_HASH=$(git rev-parse HEAD)
 if GIT_LATEST_TAG=$(git describe --exact-match --tags HEAD 2>/dev/null); then
-    echo "commit=$GIT_COMMIT_HASH ($GIT_LATEST_TAG)" >> "$RELEASE_FILE"
+    echo "commit=$GIT_COMMIT_HASH ($GIT_LATEST_TAG)" >> "$TEMP_RELEASE_FILE"
 elif GIT_BRANCH=$(git symbolic-ref --short --quiet HEAD); then
-    echo "commit=$GIT_COMMIT_HASH ($GIT_BRANCH)" >> "$RELEASE_FILE"
+    echo "commit=$GIT_COMMIT_HASH ($GIT_BRANCH)" >> "$TEMP_RELEASE_FILE"
 else
-    echo "commit=$GIT_COMMIT_HASH" >> "$RELEASE_FILE"
+    echo "commit=$GIT_COMMIT_HASH" >> "$TEMP_RELEASE_FILE"
 fi
 
-echo "date=`git log -1 --format='%ai' HEAD`" >> "$RELEASE_FILE"
+echo "date=`git log -1 --format='%ai' HEAD`" >> "$TEMP_RELEASE_FILE"
 
-if cmp "$RELEASE_FILE" "$RELEASE_PATH" >/dev/null; then
+# We add the latest commit hash to the release file which is misleading if we're pulling in modified files.
+if git status --porcelain 2>/dev/null | egrep '^ M|^M' >/dev/null; then
+    echo "WARNING Not all of your changes have been committed!" >&2
+    echo >&2
+    echo "warning=locally modified files exist" >> "$TEMP_RELEASE_FILE"
+fi
+
+if cmp "$TEMP_RELEASE_FILE" "$RELEASE_FILE" >/dev/null; then
     echo "Release information is unchanged."
 else
-    echo "Updating release information in $RELEASE_PATH"
-    cp "$RELEASE_FILE" "$RELEASE_PATH"
+    echo "Updating release information in $RELEASE_FILE"
+    cp "$TEMP_RELEASE_FILE" "$RELEASE_FILE"
 fi
-cat "$RELEASE_FILE"
+cat "$TEMP_RELEASE_FILE"
