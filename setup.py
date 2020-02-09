14c1347a98b4f101142116d005e7ca950d659dde
Tom Vogels <thomas.vogels@harrys.com>
2020-02-09 09:47:00 -0500

    Add extraction pipeline script

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="redshift_etl",
-    version="1.10.0",
+    version="1.11.0",
     author="Harry's Data Engineering and Analytics Engineering",
     description="ETL code to ferry data from PostgreSQL databases or S3 files to Redshift clusters",
     license="MIT",
@@ -20,6 +20,7 @@ setup(
     },
     scripts=[
         "python/scripts/compare_events.py",
+        "python/scripts/install_extraction_pipeline.sh",
         "python/scripts/install_pizza_load_pipeline.sh",
         "python/scripts/install_rebuild_pipeline.sh",
         "python/scripts/install_refresh_pipeline.sh",
