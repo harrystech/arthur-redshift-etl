27289ec0ccf007da7565585cf80f9e9945c0c166
Tom Vogels <thomas.vogels@harrys.com>
2021-02-27 09:41:16 -0500

    Cleanup pizza script

--- python/scripts/install_pizza_pipeline.sh
+++ python/scripts/install_pizza_pipeline.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-START_NOW=`date -u +"%Y-%m-%dT%H:%M:%S"`
+START_NOW=$(date -u +"%Y-%m-%dT%H:%M:%S")
 DEFAULT_TIMEOUT=6
 
 if [[ $# -lt 1 || $# -gt 3 || "$1" = "-h" ]]; then
-    cat <<USAGE
+  cat <<USAGE
 
 Pizza delivery! Right on time or it's free! Runs once, starting now.
 Expects S3 folder under prefix to already have all necessary manifests for source data.
 
-Usage: `basename $0` <environment> [<continue-from> [timeout]]
+Usage: $(basename "$0") <environment> [<continue-from> [timeout]]
 
 If no relation is specified, this will run the loader for all relations.
 If a "continue-from" relation is specified, then the loader will start from that relation.
@@ -17,10 +17,11 @@ If a "continue-from" relation is specified, then the loader will start from that
 If you specify ":transformations", then sources will be skipped and all transformations will be loaded.
 Optional timeout should be the number of hours pipeline is allowed to run. Defaults to $DEFAULT_TIMEOUT.
 
-Example: `basename $0` development :transformations
+Example:
+    $(basename "$0") development :transformations
 
 USAGE
-    exit 0
+  exit 0
 fi
 
 set -o errexit -o nounset
@@ -28,12 +29,12 @@ set -o errexit -o nounset
 # Verify that there is a local configuration directory
 DEFAULT_CONFIG="${DATA_WAREHOUSE_CONFIG:-./config}"
 if [[ ! -d "$DEFAULT_CONFIG" ]]; then
-    echo "Failed to find \'$DEFAULT_CONFIG\' directory."
-    echo "Make sure you are in the directory with your data warehouse setup or have DATA_WAREHOUSE_CONFIG set."
-    exit 1
+  echo >&2 "Failed to find \'$DEFAULT_CONFIG\' directory."
+  echo >&2 "Make sure you are in the directory with your data warehouse setup or have DATA_WAREHOUSE_CONFIG set."
+  exit 1
 fi
 
-PROJ_BUCKET=$( arthur.py show_value object_store.s3.bucket_name )
+PROJ_BUCKET=$(arthur.py show_value object_store.s3.bucket_name)
 PROJ_ENVIRONMENT="$1"
 CONTINUE_FROM_RELATION="${2:-*}"
 START_DATE_TIME="$START_NOW"
@@ -42,8 +43,8 @@ TIMEOUT="${4:-$DEFAULT_TIMEOUT}"
 # Verify that this bucket/environment pair is set up on s3
 BOOTSTRAP="s3://$PROJ_BUCKET/$PROJ_ENVIRONMENT/bin/bootstrap.sh"
 if ! aws s3 ls "$BOOTSTRAP" > /dev/null; then
-    echo "Check whether the bucket \"$PROJ_BUCKET\" and folder \"$PROJ_ENVIRONMENT\" exist!"
-    exit 1
+  echo "Check whether the bucket \"$PROJ_BUCKET\" and folder \"$PROJ_ENVIRONMENT\" exist!"
+  exit 1
 fi
 
 set -o xtrace
@@ -54,6 +55,8 @@ AWS_TAGS="key=user:project,value=data-warehouse key=user:sub-project,value=dw-et
 PIPELINE_NAME="ETL Pizza Loader Pipeline ($PROJ_ENVIRONMENT @ $START_DATE_TIME)"
 PIPELINE_DEFINITION_FILE="/tmp/pipeline_definition_${USER-nobody}_$$.json"
 PIPELINE_ID_FILE="/tmp/pipeline_id_${USER-nobody}_$$.json"
+
+# shellcheck disable=SC2064
 trap "rm -f \"$PIPELINE_ID_FILE\"" EXIT
 
 arthur.py render_template --prefix "$PROJ_ENVIRONMENT" pizza_pipeline > "$PIPELINE_DEFINITION_FILE"
@@ -61,15 +64,15 @@ arthur.py render_template --prefix "$PROJ_ENVIRONMENT" pizza_pipeline > "$PIPELI
 aws datapipeline create-pipeline \
     --unique-id dw-etl-pizza-pipeline \
     --name "$PIPELINE_NAME" \
-    --tags $AWS_TAGS \
-    | tee "$PIPELINE_ID_FILE"
+    --tags "$AWS_TAGS" \
+  | tee "$PIPELINE_ID_FILE"
 
-PIPELINE_ID=`jq --raw-output < "$PIPELINE_ID_FILE" '.pipelineId'`
+PIPELINE_ID=$(jq --raw-output < "$PIPELINE_ID_FILE" '.pipelineId')
 
 if [[ -z "$PIPELINE_ID" ]]; then
-    set +x
-    echo "Failed to find pipeline id in output -- pipeline probably wasn't created. Check your VPN etc."
-    exit 1
+  set +x
+  echo "Failed to find pipeline id in output -- pipeline probably wasn't created. Check your VPN etc."
+  exit 1
 fi
 
 aws datapipeline put-pipeline-definition \
