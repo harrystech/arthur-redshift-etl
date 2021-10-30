a314cfc0b34aae7ef22e87f0255f5d25cdf691d6
Tom Vogels <thomas.vogels@harrys.com>
2021-10-30 15:10:31 -0400

    Bump version for v1.54.0

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -23,8 +23,7 @@ copyright = "2017-%s, Harry's, Inc." % time.strftime("%Y")
 author = "Data Engineering at Harry's"
 
 # TODO(tom): Extract from setup.py
-version = "1.53"
-release = "1.53.0"
+version = "1.54"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-ARTHUR_VERSION = "1.53.0"
+ARTHUR_VERSION = "1.54.0"
 
 
 setup(
