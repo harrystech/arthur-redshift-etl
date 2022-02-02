3daca3b5adfc05901d49240c7f08bdbfdcf64fdd
ynaim94 <youssef.naim@harrys.com>
2022-02-01 20:26:25 -0500

    Use alias for arthur.py

--- docker/entrypoint_remote.sh
+++ docker/entrypoint_remote.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 set -o errexit -o nounset
+alias arthur.py=/opt/local/redshift_etl/venv/bin/arthur.py
+shopt -s expand_aliases
+
 export DATA_WAREHOUSE_CONFIG='./config/'
 aws s3 cp --only-show-errors --recursive "s3://$BUCKET_NAME/$ARTHUR_DEFAULT_PREFIX/config/" $DATA_WAREHOUSE_CONFIG
 aws s3 cp --only-show-errors --recursive "s3://$BUCKET_NAME/$ARTHUR_DEFAULT_PREFIX/schemas/" ./schemas
